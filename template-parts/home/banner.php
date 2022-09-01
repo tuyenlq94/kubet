3be37489cd2665769dfcd178e8d87381fce52290
tuyenlq94 <tuyenicolor@gmail.com>
2022-09-01 17:44:13 +0700

    thêm target vào thẻ a

--- template-parts/home/banner.php
+++ template-parts/home/banner.php
@@ -1,30 +1,30 @@
 <?php
-$images = rwmb_meta('image_banner');
-$group_link = rwmb_meta('group_link');
+$images     = rwmb_meta( 'image_banner' );
+$group_link = rwmb_meta( 'group_link' );
 ?>
 <section class="banner">
-	<?php foreach($images as $image ) : ?>
-		<img src="<?= esc_url($image['full_url']);?>" alt="">
+	<?php foreach ( $images as $image ) : ?>
+		<img src="<?= esc_url( $image['full_url'] );?>" alt="">
 	<?php endforeach; ?>
 </section>
 <section class="section-link">
 	<div class="container">
 		<div class="section-link__wrap">
-			<?php 
-			foreach($group_link as $value) : 
+			<?php
+			foreach ( $group_link as $value ) :
 				$image = $value['image'];
 				$title = $value['title'];
-				$link = $value['link'];	
-			?>
+				$link  = $value['link'];
+				?>
 				<div class="section-link__item">
 					<div class="section-link__image">
-						<img src="<?= esc_url(wp_get_attachment_image_url( $image, 'full', false ));?>" alt="<?= esc_html($title);?>">
+						<img src="<?= esc_url( wp_get_attachment_image_url( $image, 'full', false ) );?>" alt="<?= esc_html( $title );?>">
 					</div>
 					<div class="section-link__title">
-						<a href="<?= esc_url($link);?>"><?= esc_html($title);?></a>
+						<a href="<?= esc_url( $link );?>" target="_blank" rel="nofollow"><?= esc_html( $title );?></a>
 					</div>
 				</div>
-			<?php endforeach;?>
+			<?php endforeach; ?>
 		</div>
 	</div>
 </section>
